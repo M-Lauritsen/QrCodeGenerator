8bb252058b80f718b979e5d239ea20ca3228e967
Michael Lauritsen <i30288741@live.dk>
2018-11-23 10:30:19 +0100

    Revert "QR Code Generator, side project for a co worker"
    This reverts commit 1326cf547da3181f23f66850230ccafd8830a4f9.

--- QrCodeGenerator/Program.cs
+++ QrCodeGenerator/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Reflection;
 
 namespace QrCodeGenerator
 {
@@ -17,29 +16,7 @@ namespace QrCodeGenerator
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            AppDomain.CurrentDomain.AssemblyResolve += new ResolveEventHandler(CurrentDrawing_AssemblyResolve);
-            AppDomain.CurrentDomain.AssemblyResolve += new ResolveEventHandler(CurrentBarcode_AssemblyResolve);
             Application.Run(new Form1());
-        }
-
-        private static Assembly CurrentDrawing_AssemblyResolve(object sender, ResolveEventArgs args)
-        {
-            using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("EmbedAssembly.System.Drawing.Common.dll"))
-            {
-                byte[] assemblyData = new byte[stream.Length];
-                stream.Read(assemblyData, 0, assemblyData.Length);
-                return Assembly.Load(assemblyData);
-            }
-        }
-
-        private static Assembly CurrentBarcode_AssemblyResolve(object sender, ResolveEventArgs args)
-        {
-            using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("EmbedAssembly.Zen.Barcode.Core.dll"))
-            {
-                byte[] assemblyData = new byte[stream.Length];
-                stream.Read(assemblyData, 0, assemblyData.Length);
-                return Assembly.Load(assemblyData);
-            }
         }
     }
 }
